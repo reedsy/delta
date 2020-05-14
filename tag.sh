106249ca7ed4a285b8e3711585f0ae3a27f211ee
Alec Gibson <aleckgibson@gmail.com>
2020-05-14 11:41:19 +0100

    Fix tag.sh

--- tag.sh
+++ tag.sh
@@ -16,11 +16,11 @@ else
   echo "Deploying version $VERSION"
 fi
 
-echo '!/lib' >> .gitignore
+echo '!/dist' >> .gitignore
 
 git checkout -b release-$VERSION
 git add .gitignore
-git add --all lib/
+git add --all dist/
 git commit --message "Release version $VERSION"
 git tag $VERSION
 git push origin refs/tags/$VERSION
