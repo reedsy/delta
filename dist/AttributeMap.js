f3f78531f67538ad42046549d229ec81c0db2985
GitHub Action <github@reedsy.com>
2023-08-03 10:50:37 +0000

    Release version 5.1.0-reedsy.3.0.0

---
+++ dist/AttributeMap.js
@@ -0,0 +1,126 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rfdc = require("rfdc");
+const isEqual = require("fast-deep-equal");
+const cloneDeep = rfdc();
+function isObject(value) {
+    return value === Object(value) && !Array.isArray(value);
+}
+function isEmptyObject(value) {
+    return isObject(value) && !Object.keys(value).length;
+}
+function removeNullOrEmptyObjects(value) {
+    if (!value || !isObject(value))
+        return;
+    for (const key in value) {
+        const k = key;
+        removeNullOrEmptyObjects(value[k]);
+        if (value[k] == null || isEmptyObject(value[k]))
+            delete value[k];
+    }
+}
+var AttributeMap;
+(function (AttributeMap) {
+    function compose(a = {}, b = {}, keepNull = false) {
+        if (typeof a !== 'object') {
+            a = {};
+        }
+        if (typeof b !== 'object') {
+            b = {};
+        }
+        const attributes = cloneDeep(b);
+        for (const key in a) {
+            const aValue = a[key];
+            const attrValue = attributes[key];
+            if (isObject(aValue) && isObject(attrValue)) {
+                attributes[key] = compose(aValue, attrValue, keepNull);
+            }
+        }
+        if (!keepNull)
+            removeNullOrEmptyObjects(attributes);
+        for (const key in a) {
+            if (a[key] !== undefined && b[key] === undefined) {
+                attributes[key] = a[key];
+            }
+        }
+        return Object.keys(attributes).length > 0 ? attributes : undefined;
+    }
+    AttributeMap.compose = compose;
+    function diff(a = {}, b = {}) {
+        if (typeof a !== 'object') {
+            a = {};
+        }
+        if (typeof b !== 'object') {
+            b = {};
+        }
+        const attributes = Object.keys(a)
+            .concat(Object.keys(b))
+            .reduce((attrs, key) => {
+            if (!isEqual(a[key], b[key])) {
+                const aValue = a[key];
+                const bValue = b[key];
+                if (b[key] === undefined) {
+                    attrs[key] = null;
+                }
+                else if (isObject(aValue) && isObject(bValue)) {
+                    attrs[key] = diff(aValue, bValue);
+                }
+                else {
+                    attrs[key] = bValue;
+                }
+            }
+            return attrs;
+        }, {});
+        return Object.keys(attributes).length > 0 ? attributes : undefined;
+    }
+    AttributeMap.diff = diff;
+    function invert(attr = {}, base = {}) {
+        attr = attr || {};
+        const baseInverted = Object.keys(base).reduce((memo, key) => {
+            const attrValue = attr[key];
+            const baseValue = base[key];
+            if (!isEqual(baseValue, attrValue) && attrValue !== undefined) {
+                if (isObject(attrValue) && isObject(baseValue)) {
+                    memo[key] = invert(attrValue, baseValue);
+                }
+                else {
+                    memo[key] = base[key];
+                }
+            }
+            return memo;
+        }, {});
+        return Object.keys(attr).reduce((memo, key) => {
+            if (attr[key] !== base[key] && base[key] === undefined) {
+                memo[key] = null;
+            }
+            return memo;
+        }, baseInverted);
+    }
+    AttributeMap.invert = invert;
+    function transform(a, b, priority = false) {
+        if (typeof a !== 'object') {
+            return b;
+        }
+        if (typeof b !== 'object') {
+            return undefined;
+        }
+        if (!priority) {
+            return b; // b simply overwrites us without priority
+        }
+        const attributes = Object.keys(b).reduce((attrs, key) => {
+            const aValue = a[key];
+            const bValue = b[key];
+            if (aValue === undefined) {
+                attrs[key] = bValue; // null is a valid value
+            }
+            else if (isObject(aValue) && isObject(bValue)) {
+                attrs[key] = transform(aValue, bValue, true);
+            }
+            return attrs;
+        }, {});
+        return Object.keys(attributes).length > 0 ? attributes : undefined;
+    }
+    AttributeMap.transform = transform;
+})(AttributeMap || (AttributeMap = {}));
+exports.default = AttributeMap;
+//# sourceMappingURL=AttributeMap.js.map
