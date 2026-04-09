605d701b7a51d2ddec123a80f820a68159df80d3
GitHub Action <github@reedsy.com>
2026-04-09 13:14:34 +0000

    Release version 5.1.0-reedsy.4.0.0

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import { AttributeMap } from './AttributeMap';
+import { Delta } from './Delta';
+import { Op } from './Op';
+import { OpIterator } from './OpIterator';
+export { AttributeMap, Delta, Op, OpIterator };
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OpIterator = exports.Op = exports.Delta = exports.AttributeMap = void 0;
+const AttributeMap_1 = require("./AttributeMap");
+Object.defineProperty(exports, "AttributeMap", { enumerable: true, get: function () { return AttributeMap_1.AttributeMap; } });
+const Delta_1 = require("./Delta");
+Object.defineProperty(exports, "Delta", { enumerable: true, get: function () { return Delta_1.Delta; } });
+const Op_1 = require("./Op");
+Object.defineProperty(exports, "Op", { enumerable: true, get: function () { return Op_1.Op; } });
+const OpIterator_1 = require("./OpIterator");
+Object.defineProperty(exports, "OpIterator", { enumerable: true, get: function () { return OpIterator_1.OpIterator; } });
+//# sourceMappingURL=index.js.map
